6040f1fe37449580af4bc6120e4f6fd61f6815ca
Huck <evanallen51@gmail.com>
2017-11-21 07:45:22 -0600

    initial commit with create react app and create express app

---
+++ campfire/server.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+var server = express();
+
+// view engine setup
+server.set('views', path.join(__dirname, 'views'));
+server.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//server.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+server.use(logger('dev'));
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: false }));
+server.use(cookieParser());
+server.use(express.static(path.join(__dirname, 'public')));
+
+server.use('/', index);
+server.use('/users', users);
+
+// catch 404 and forward to error handler
+server.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+server.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.server.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = server;
